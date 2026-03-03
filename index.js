9174f692ac9ed4f02e81fdf2be64d848cde379cc
Zbhoot <zbhoot6@gmail.com>
2026-03-04 00:34:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ client.on("messageCreate", async (message) => {
   if (command === "giveaway") {
 
   // Only Espadas can start
-  if (!message.member.roles.cache.has(ESPADA_PARENT_ROLE_ID)) {
+  if (!message.member.roles.cache.has(1475606720356290581)) {
     return message.reply("Only Espadas can start a giveaway.");
   }
 
